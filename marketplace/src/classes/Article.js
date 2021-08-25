04d20cbefcfe94591d1d4a93e01e21a06fc569d7
Axel Cuevas <axelcoevas@gmail.com>
2021-08-25 11:58:47 -0500

    Creando clases del diagrama UML

---
+++ marketplace/src/classes/Article.js
@@ -0,0 +1,13 @@
+export class Article {
+  constructor(productId, productName, productImage, price, quantity) {
+    this.productId = productId;
+    this.productName = productName;
+    this.productImage = productImage;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  showItem() {
+
+  }
+}
