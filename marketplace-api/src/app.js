5af64ef8d84b6ae4e520a7638cc98cc839eff678
Ele Cabrera <jai690@hotmail.com>
2021-09-05 11:39:18 -0500

    npm

--- marketplace-api/src/app.js
+++ marketplace-api/src/app.js
@@ -13,8 +13,7 @@ app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs',
-    helpers: require('./lib/handlebars')
+    extname: '.hbs'
 }));
 app.set('view.engine', '.hbs');
 
