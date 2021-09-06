883d972589cae7123c6ff07c4ba9bb05b4f682a4
Antonio Labra <antonio_labra@hotmail.com>
2021-09-05 22:22:33 -0500

    Agregando al index.js los routes correspondientes

--- marketplace-api/src/routes/index.js
+++ marketplace-api/src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render('../views/index.hbs');
 });
 
+// USERS ROUTE
+router.use("/users", require("./users"));
+router.use("/sellers", require("./sellers"));
+router.use("/buyers", require("./buyers"));
+router.use("/articles", require("./articles"));
+router.use("/catalogs", require("./catalogs"));
+router.use("/reviews", require("./reviews"));
+router.use("/purchases", require("./purchases"));
+router.use("/purchasesDetail", require("./purchasesdetail"));
 module.exports = router;
