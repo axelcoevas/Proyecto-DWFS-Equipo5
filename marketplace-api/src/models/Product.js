d9ffb5cf4974ca96f7d5ba21f89d3bcf319770d5
Axel Cuevas <axelcoevas@gmail.com>
2021-09-21 16:41:15 -0500

    Completando los modelos

---
+++ marketplace-api/src/models/Product.js
@@ -0,0 +1,31 @@
+// class Product {
+//   constructor(productId, productName, productImage, price, quantity) {
+//     this.productId = productId;
+//     this.productName = productName;
+//     this.productImage = productImage;
+//     this.price = price;
+//     this.quantity = quantity;
+//   }
+// }
+
+// module.exports = Product;
+
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  price: Number,
+  quantity: Number
+}, { collection: 'Products', timestamps: true });
+
+ProductSchema.methods.publicData = () => {
+  return {
+    name: this.name,
+    image: this.image,
+    price: this.price,
+    quantity: this.quantity
+  };
+};
+
+mongoose.model('Product', ProductSchema);
