a2ff4252b23c92d6367c3c5147253bca9dc3b974
Axel Cuevas <axelcoevas@gmail.com>
2021-09-26 18:58:27 -0500

    Acomodando los archivos para deploy

--- marketplace-api/src/config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export PORT=4001
-export SECRET='secret'
-export MONGO_URI='mongodb+srv://introabd:introabd1234@cluster0.39y8b.mongodb.net/Bazaar?retryWrites=true&w=majority'
