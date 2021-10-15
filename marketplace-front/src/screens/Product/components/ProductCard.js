e566b905bf3d0b931eaf9a4055bb12f7bd833ff0
Diego Melo <diego_14melo@hotmail.com>
2021-10-15 17:12:38 -0500

    wip: Producto

---
+++ marketplace-front/src/screens/Product/components/ProductCard.js
@@ -0,0 +1,21 @@
+import Avatar from '@mui/material/Avatar';
+import Paper from '@mui/material/Paper';
+
+const ProductCard = props => {
+    return <Paper sx={{width: '30%'}}>
+        <div style={{padding: 20}}>
+            <img src={props.image} style={{width: '100%'}} />
+        </div>
+        
+        <div className='center' style={{justifyContent: 'space-between', margin: 20}}>
+            <Avatar  src={props.seller_image} />
+            <div style={{fontSize: "1.25rem"}}>
+                <p className='product-name'>{props.name}</p>
+                <p className="price">{props.price}</p>
+            </div>
+        </div>
+
+    </Paper>
+}
+
+export default ProductCard;
