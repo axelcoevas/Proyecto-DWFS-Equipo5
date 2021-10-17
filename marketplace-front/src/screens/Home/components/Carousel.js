fa4f7476dc012827d43e4047884b6d330b232e72
Roger Magaña <11rogelio@hotmail.es>
2021-10-16 19:41:18 -0500

    Componente ArrowButton reutilizable y Animación agregadas a Carrusel

--- marketplace-front/src/screens/Home/components/Carousel.js
+++ marketplace-front/src/screens/Home/components/Carousel.js
@@ -1,71 +1,70 @@
 import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-// import Slide from '@mui/material/Slide';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import Slide from '@mui/material/Slide';
 import CarouselSlide from './CarouselSlide';
+import ArrowButton from './ArrowButton';
+
+const SLIDES_ITEMS = [
+	{
+		title: 'Imagen 1',
+		image: 'https://st3.depositphotos.com/15582768/18733/v/1600/depositphotos_187339500-stock-illustration-flat-colorful-design-concept-marketplace.jpg',
+	},
+	{
+		title: 'Imagen 2',
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT5s2MB9UwVF2URaOoCVD5wvM8r7HrrZqC1Q&usqp=CAU',
+	},
+	{
+		title: 'Imagen 3',
+		image: 'https://www.7apa.com/wp-content/uploads/2017/08/Web-Banner-MarketPlace-700x360px.jpg',
+	},
+	{
+		title: 'Imagen 4',
+		image: 'https://67degreescdn.co.uk/42/6/15765255325df7dedc05ada_blog-banner.jpg?width=479&height=359&crop=auto',
+	},
+	{
+		title: 'Imagen 5',
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxqBwhhRX9m4at5jngj3ILQzrhwKoyET30w&usqp=CAU',
+	},
+];
 
 function Carousel() {
-	const SLIDES_ITEMS = [
-		{
-			title: 'Imagen 1',
-			image: 'https://st3.depositphotos.com/15582768/18733/v/1600/depositphotos_187339500-stock-illustration-flat-colorful-design-concept-marketplace.jpg',
-		},
-		{
-			title: 'Imagen 2',
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT5s2MB9UwVF2URaOoCVD5wvM8r7HrrZqC1Q&usqp=CAU',
-		},
-		{
-			title: 'Imagen 3',
-			image: 'https://www.7apa.com/wp-content/uploads/2017/08/Web-Banner-MarketPlace-700x360px.jpg',
-		},
-		{
-			title: 'Imagen 4',
-			image: 'https://67degreescdn.co.uk/42/6/15765255325df7dedc05ada_blog-banner.jpg?width=479&height=359&crop=auto',
-		},
-		{
-			title: 'Imagen 5',
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxqBwhhRX9m4at5jngj3ILQzrhwKoyET30w&usqp=CAU',
-		},
-	];
+    // Navigation among elements
+	const [index, setIndex] = useState(0);
+	const content = SLIDES_ITEMS[index];
+	const numSlides = SLIDES_ITEMS.length;
+
+	const [slideIn, setSlideIn] = useState(true);
+	const [slideDirection, setSlideDirection] = useState('down');
+
+	const onArrowClick = direction => {
+		const increment = direction === 'left' ? -1 : 1;
+		const newIndex = (index + increment + numSlides) % numSlides;
+
+		const oppDirection = direction === 'left' ? 'right' : 'left';
+		setSlideDirection(direction);
+		setSlideIn(false);
+
+		setTimeout(() => {
+			setSlideDirection(oppDirection);
+			setIndex(newIndex);
+			setSlideIn(true);
+		}, 500);
+	};
 
 	return (
 		<>
-			<Button
-				sx={{
-					margin: '0 -40px',
-					width: '6%',
-					height: '20%',
-					background: ' #3F3F3F',
-					color: '#FFF',
-					borderRadius: '8px',
-					transform: 'matrix(1, -0.01, -0.01, -1, 0, 0)',
-					':hover': {
-						color: '#3F3F3F',
-						background: '#FFF',
-					},
-				}}>
-				<ArrowBackIosNewIcon />
-			</Button>
-
-			<CarouselSlide content={SLIDES_ITEMS[3]} />
-
-			<Button
-				sx={{
-					margin: '0 -40px',
-					width: '6%',
-					height: '20%',
-					background: ' #3F3F3F',
-					color: '#FFF',
-					borderRadius: '8px',
-					transform: 'matrix(1, -0.01, -0.01, -1, 0, 0)',
-					':hover': {
-						color: '#3F3F3F',
-						background: '#FFF',
-					},
-				}}>
-				<ArrowForwardIosIcon />
-			</Button>
+			<ArrowButton
+				direction="left"
+				onArrowClick={() => onArrowClick('left')}
+			/>
+			<Slide in={slideIn} direction={slideDirection}>
+				<div style={{ width: '100%' }}>
+					<CarouselSlide content={content} />
+				</div>
+			</Slide>
+			<ArrowButton
+				direction="right"
+				onArrowClick={() => onArrowClick('right')}
+			/>
 		</>
 	);
 }
--- marketplace-front/src/screens/Home/index.js
+++ marketplace-front/src/screens/Home/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 
 import Carousel from './components/Carousel';
 
 const Home = () => {
-	
 
 	return (
 		<Container sx={{backgroundColor: '#E5E5E5', margin: 0}} maxWidth="xl">
