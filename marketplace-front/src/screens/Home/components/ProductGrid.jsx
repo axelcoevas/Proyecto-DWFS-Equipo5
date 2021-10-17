5b3dee6bb447f36f874d3211c6bef1ea872b824a
Axel Cuevas <axelcoevas@gmail.com>
2021-10-17 09:19:19 -0500

    Starting ProductGrid

---
+++ marketplace-front/src/screens/Home/components/ProductGrid.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Grid } from '@mui/material';
+import ProductPreview from './ProductPreview';
+import { Box } from '@mui/system';
+
+const product = {
+    id: 100000134,
+    name: "Red bomber jacket XS",
+    image: "https://m.media-amazon.com/images/I/81Uh6lgjPkL._AC_SX569_.jpg",
+    price: 1250,
+    quantity: 2,
+    // props not on db model
+    seller: "Seller name",
+    seller_image: "https://pbs.twimg.com/profile_images/1086332409677660160/Lorn8QZ2.jpg",
+};
+
+const products = [product, product, product, product, product];
+
+const ProductGrid = () => {
+    const classes = makeStyles(theme => ({
+        ProductGrid: {
+            padding: '1em',
+            display: 'flex'
+        },
+        ProductPreview: {
+            width: 'auto',
+            flex: '1'
+        }
+    }))();
+
+    return (
+        <div className={classes.ProductGrid}>
+            {products.map((product) => (<ProductPreview className={classes.ProductPreview} product={product} />))}
+        </div>
+    );
+};
+
+export default ProductGrid;
---
+++ marketplace-front/src/screens/Home/components/ProductPreview.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Paper, Avatar } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/system';
+
+const ProductPreview = ({ product }) => {
+    const classes = makeStyles(theme => ({
+        ProductPreviewContainer: {
+            margin: '1em 1.5em',
+            width: 'auto'
+        },
+        ProductName: {
+            fontSize: '1rem',
+            textAlign: 'right'
+        },
+        ProductPrice: {
+            fontSize: '1.25rem',
+            textAlign: 'right'
+        },
+        ProductImage: {
+            height: '14vh'
+        },
+        ImageContainer: {
+            padding: '1em'
+        },
+        DescriptionContainer: {
+            justifyContent: 'space-between',
+            backgroundColor: '#ffc1bd',
+            padding: '0 1em'
+        }
+    }))();
+
+    return (
+        <Paper className={classes.ProductPreviewContainer}>
+            <div className={`center ${classes.ImageContainer}`}>
+                <img className={classes.ProductImage} src={product.image} alt={product.name} />
+            </div>
+
+            <div className={`center ${classes.DescriptionContainer}`}>
+                <Avatar src={product.seller_image} />
+                <div>
+                    <p className={`product-name ${classes.ProductName}`}>{product.name}</p>
+                    <p className={`price ${classes.ProductPrice}`}>{product.price}</p>
+                </div>
+            </div>
+
+        </Paper>
+    );
+};
+
+export default ProductPreview;
