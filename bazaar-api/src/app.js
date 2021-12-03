6d4ee5ae5d389a9ec9691803c5c5737ca92bbf05
Diego Melo <diego_14melo@hotmail.com>
2021-12-02 18:08:59 -0600

    fix: habilitando cors

--- bazaar-api/src/app.js
+++ bazaar-api/src/app.js
@@ -1,45 +1,48 @@
 //Requerimientos
-const express = require('express');
+const express = require("express");
 const app = express();
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
+const cors = require("cors");
 //  Mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(
-    MONGO_URI,
-    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 //  Body Parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cors());
+
 //  Models
-require('./models/Catalog');
-require('./models/Product');
-require('./models/Purchase');
-require('./models/Review');
-require('./models/User');
+require("./models/Catalog");
+require("./models/Product");
+require("./models/Purchase");
+require("./models/Review");
+require("./models/User");
 
 //  Passport
-require('./config/passport');
+require("./config/passport");
 
 //  Routes
 app.use("/api/v1", require("./routes"));
 
 //  Start the server
 app.listen(PORT, () => {
-    console.log("Server listening on port", PORT);
+  console.log("Server listening on port", PORT);
 });
 
 // SEED
 
-if (process.env.NODE_ENV === 'development') {
-    const seed = require("../fakeseed.js");
-    seed();
+if (process.env.NODE_ENV === "development") {
+  const seed = require("../fakeseed.js");
+  seed();
 }
-
